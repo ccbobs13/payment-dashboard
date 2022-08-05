04cac0c10dcb8b99d1104ca4c355ae17822f561f
Samyr Christian Yentema Thiombiano <>
2022-08-05 15:37:55 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ cd dist
 git init
 git add .
 git commit -m 'deploy'
-git push -f https://github.com/ccbobs13/vue-dashboard.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com/ccbobs13/vue-dashboard.git master:gh-pages
 
 cd -
